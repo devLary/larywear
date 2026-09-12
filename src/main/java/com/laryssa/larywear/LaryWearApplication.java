8121ba4f68cdb77b17dd42a01e3b97228407a9ef
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-09-12 13:36:36 -0300

    Initial commit

---
+++ src/main/java/com/laryssa/larywear/LaryWearApplication.java
@@ -0,0 +1,13 @@
+package com.laryssa.larywear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaryWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaryWearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laryssa/larywear/LarywearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laryssa.larywear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarywearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
