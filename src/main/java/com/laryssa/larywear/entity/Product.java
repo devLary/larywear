822da0c67cd44d81e5962265745b3b25e03ec6e6
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-09-14 18:28:58 -0300

    feat: configure database and product entity

--- src/main/java/com/laryssa/larywear/entity/Product.java
+++ src/main/java/com/laryssa/larywear/entity/Product.java
@@ -1,12 +1,29 @@
 package com.laryssa.larywear.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotNull
+    @PositiveOrZero
     private BigDecimal price;
+
+    @PositiveOrZero
     private int stock;
+
 
     public Product() {
     }
@@ -20,7 +37,6 @@ public class Product {
     public Long getId() {
         return id;
     }
-
 
     public String getName() {
         return name;
